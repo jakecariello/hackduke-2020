26500085adb82fa731f4f1ea7888f03c3360570b
Luiza Wolf <annaluiza.wolf@duke.edu>
2020-12-05 14:47:06 -0500

    init commit - functioning API calls to FoodData Central to get ingredients by meal name

---
+++ usda.py
@@ -0,0 +1,38 @@
+import requests
+import json
+import config
+from urllib.parse import urlencode
+
+BASE_URL = "https://api.nal.usda.gov/fdc/v1"
+
+"""
+Searches FoodData Central for foods that match keyword description 
+RETURNS: top match for keyword search, filtered by "survey" datatype (as opposed to "branded")
+"""
+def searchFoods(keywords):
+    #filters by survey, vs branded foods
+    query = urlencode({"query":keywords, "dataType":"Survey (FNDDS)"})
+    getfoods_url = BASE_URL + "/foods/search?" + "api_key=" + config.USDA_API_KEY + "&" + query
+    response = requests.get(getfoods_url)
+    jsonresponse = json.loads(response.text)
+    foods = jsonresponse["foods"]
+    top_match = foods[0]
+    return top_match
+
+"""
+RETURNS: ingredients for survey food by fdc_id
+"""
+def getIngredients(fdc_id):
+    url = BASE_URL + "/food/" + str(fdc_id) + "?api_key=" + config.USDA_API_KEY 
+    response = requests.get(url)
+    jsonresponse = json.loads(response.text)
+    ingredients = jsonresponse["inputFoods"] #"inputFoods" throws error for survey foods, which have "ingredients"
+    ingredients = [x['ingredientDescription'] for x in ingredients]
+    return ingredients
+
+
+# Example
+meal_name = "Vegetable Lasagna"
+food_match = searchFoods(meal_name)
+meal_ingredients = getIngredients(food_match["fdcId"])
+print(meal_ingredients)
