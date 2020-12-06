0e87b729fe904b47482ca0ec516113880d051116
Luiza Wolf <annaluiza.wolf@duke.edu>
2020-12-05 19:22:17 -0500

    allergycheck function interacting with allergies and ingredientkeyword tables

--- usda.py
+++ usda.py
@@ -2,6 +2,7 @@ import requests
 import json
 import config
 from urllib.parse import urlencode
+from app import Allergy, IngredientKeyword, db
 
 BASE_URL = "https://api.nal.usda.gov/fdc/v1"
 
@@ -26,27 +27,33 @@ def getIngredients(fdc_id):
     url = BASE_URL + "/food/" + str(fdc_id) + "?api_key=" + config.USDA_API_KEY 
     response = requests.get(url)
     jsonresponse = json.loads(response.text)
+    print(response.status_code)
     ingredients = jsonresponse["inputFoods"] #"inputFoods" throws error for survey foods, which have "ingredients"
     ingredients = [x['ingredientDescription'] for x in ingredients]
     return ingredients
 
 def allergyCheck(allergies, ingredients):
-    allergies_found = list()
+    allergies_found = set()
     ingredients_str = ' '.join(ingredients).lower()
     for allergy in allergies:
-        #get ingredient keywords for each allergy
-        #see if any of the ingredient keywords show up in ingredients_str
-        
-        #what about the other direction?
 
-        
         if allergy.lower() in ingredients_str:
-            allergies_found.append(allergy)
+            allergies_found.add(allergy)
+
+        keywords = []
+        allergy_obj = Allergy.query.filter_by(name=allergy.lower()).first()
+        if allergy_obj:
+            keywords = IngredientKeyword.query.filter_by(allergy_id=allergy_obj.id)
+
+        for keyword in keywords:
+            ingredient = keyword.keyword
+            if ingredient.lower() in ingredients_str:
+                allergies_found.add(allergy)
+
     return allergies_found
 
-
 # Example
-meal_name = "Vegetable Lasagna"
+meal_name = "Pad Thai"
 food_match = searchFoods(meal_name)
 meal_ingredients = getIngredients(food_match["fdcId"])
 my_allergies = ["Dairy","Egg","Gluten","Bananas","Mustard"]
